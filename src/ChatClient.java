dba7d31263d33bbfe5b2dc4ae1c8f8ee81117913
Hasindu1 <hasiyadahanayake@gmail.com>
2020-05-29 00:24:00 +0530

    initial commit

---
+++ src/ChatClient.java
@@ -0,0 +1,237 @@
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ * A simple Swing-based client for the chat server. Graphically it is a frame
+ * with a text field for entering messages and a textarea to see the whole
+ * dialog.
+ *
+ * The client follows the Chat Protocol which is as follows. When the server
+ * sends "SUBMITNAME" the client replies with the desired screen name. The
+ * server will keep sending "SUBMITNAME" requests as long as the client submits
+ * screen names that are already in use. When the server sends a line beginning
+ * with "NAMEACCEPTED" the client is now allowed to start sending the server
+ * arbitrary strings to be broadcast to all chatters connected to the server.
+ * When the server sends a line beginning with "MESSAGE " then all characters
+ * following this string should be displayed in its message area.
+ */
+//IT18153828 H.Y.D.Dahanayake
+public class ChatClient {
+
+	BufferedReader in;
+	PrintWriter out;
+
+	JFrame frame = new JFrame("Chatter");
+	JTextField textField = new JTextField(40);
+	JTextArea messageArea = new JTextArea(8, 40);
+	JButton button;
+	// TODO: Add a list box
+
+	JList jList;
+	DefaultListModel<String> dm = new DefaultListModel<String>();
+
+	List<String> multicastUsers;
+
+	/**
+	 * Constructs the client by laying out the GUI and registering a listener with
+	 * the textfield so that pressing Return in the listener sends the textfield
+	 * contents to the server. Note however that the textfield is initially NOT
+	 * editable, and only becomes editable AFTER the client receives the
+	 * NAMEACCEPTED message from the server.
+	 */
+	public ChatClient() {
+
+		// Layout GUI
+		textField.setEditable(false);
+		messageArea.setEditable(false);
+		jList = new JList();
+		jList.setModel(dm);
+
+		// To confirm the selection of multicast users
+		button = new JButton("Get Selected multicast users");
+
+		// To store the list of multicast users
+		multicastUsers = new ArrayList<String>();
+
+		getSelected();
+		frame.getContentPane().add(textField, "North");
+		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+		frame.getContentPane().add(jList, BorderLayout.EAST);
+		button.setSize(100, 100);
+		frame.getContentPane().add(button, BorderLayout.PAGE_END);
+		frame.pack();
+
+		// TODO: You may have to edit this event handler to handle point to point
+		// messaging,
+		// where one client can send a message to a specific client. You can add some
+		// header to
+		// the message to identify the recipient. You can get the receipient name from
+		// the listbox.
+		textField.addActionListener(new ActionListener() {
+			/**
+			 * Responds to pressing the enter key in the textfield by sending the contents
+			 * of the text field to the server. Then clear the text area in preparation for
+			 * the next message.
+			 */
+			public void actionPerformed(ActionEvent e) {
+				if (multicastUsers.size() == 0) {
+					out.println(textField.getText());
+					textField.setText("");
+					// Selecting the multicast messages
+				} else {
+					out.println("Multi" + textField.getText());
+					textField.setText("");
+
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * Prompt for and return the address of the server.
+	 */
+	private String getServerAddress() {
+		return JOptionPane.showInputDialog(frame, "Enter IP Address of the Server:", "Welcome to the Chatter",
+				JOptionPane.QUESTION_MESSAGE);
+	}
+
+	/**
+	 * Prompt for and return the desired screen name.
+	 */
+	private String getName() {
+		return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
+				JOptionPane.PLAIN_MESSAGE);
+	}
+
+	private void getSelected() {
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Object[] values = jList.getSelectedValues();
+				for (int i = 0; i < values.length; i++) {
+					System.out.println("indices" + values[i]);
+					multicastUsers.add(values[i].toString());
+				}
+			}
+		});
+	}
+
+	/**
+	 * Connects to the server then enters the processing loop.
+	 */
+	private void run() throws IOException {
+
+		// Make connection and initialize streams
+		String serverAddress = getServerAddress();
+		Socket socket = new Socket(serverAddress, 9001);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(), true);
+
+		// Process all messages from server, according to the protocol.
+
+		// TODO: You may have to extend this protocol to achieve task 9 in the lab sheet
+		while (true) {
+
+			String line = in.readLine();
+
+			if (line.startsWith("SUBMITNAME")) {
+
+				out.println(getName());
+
+			} else if (line.startsWith("NAMEACCEPTED")) {
+
+				textField.setEditable(true);
+
+			}
+			// printing all the logged in clients
+			else if (line.startsWith("USERS")) {
+
+				// adding a client for the jlist for the first time
+				if (jList.getModel().getSize() == 0) {
+
+					dm.addElement(line.substring(5));
+
+				} else {
+
+					boolean invalid = false;
+					for (int i = 0; i < jList.getModel().getSize(); i++) {
+						System.out.println("Size " + jList.getSize());
+						System.out.println("Element in jList" + jList.getModel().getElementAt(i));
+						if ((jList.getModel().getElementAt(i).equals(line.substring(5)))) {
+							System.out.println("Adding eleemt " + line.substring(5));
+
+							invalid = true;
+
+						}
+					}
+					if (!invalid) {
+						dm.addElement(line.substring(5));
+
+					}
+				}
+				// Displaying the broadcast message
+			} else if (line.startsWith("MESSAGE")) {
+
+				messageArea.append(line.substring(8) + "\n");
+				// displaying the point to point message
+			} else if (line.startsWith("PointToPoint")) {
+
+				messageArea.append(line.substring(12) + "\n");
+				// sending the list of multicast users for the server
+			} else if (line.startsWith("Multicast") && multicastUsers.size() > 0) {
+
+				for (String tempUser : multicastUsers) {
+					out.println(tempUser);
+				}
+				// To denote end of the multicast users list
+				out.println("END");
+				// displaying the multi cast messages
+			} else if (line.startsWith("MultiCastMSG")) {
+
+				messageArea.append(line.substring(12) + "\n");
+				multicastUsers.clear();
+				// removing the logged off user from the jLsit
+			} else if (line.startsWith("RemoveUsers")) {
+				for (int i = 0; i < jList.getModel().getSize(); i++) {
+					if ((jList.getModel().getElementAt(i).equals(line.substring(11)))) {
+						dm.removeElement(line.substring(11));
+					}
+				}
+			}
+
+		}
+
+	}
+
+	/**
+	 * Runs the client as an application with a closeable frame.
+	 */
+	public static void main(String[] args) throws Exception {
+		ChatClient client = new ChatClient();
+		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.frame.setVisible(true);
+		client.run();
+	}
+}
---
+++ src/ChatServer.java
@@ -0,0 +1,288 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * A multithreaded chat room server. When a client connects the server requests
+ * a screen name by sending the client the text "SUBMITNAME", and keeps
+ * requesting a name until a unique one is received. After a client submits a
+ * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
+ * from that client will be broadcast to all other clients that have submitted a
+ * unique screen name. The broadcast messages are prefixed with "MESSAGE ".
+ *
+ * Because this is just a teaching example to illustrate a simple chat server,
+ * there are a few features that have been left out. Two are very useful and
+ * belong in production code:
+ *
+ * 1. The protocol should be enhanced so that the client can send clean
+ * disconnect messages to the server.
+ *
+ * 2. The server should do some logging.
+ */
+//IT18153828 H.Y.D.Dahanayake
+public class ChatServer {
+
+	/**
+	 * The port that the server listens on.
+	 */
+	private static final int PORT = 9001;
+
+	/**
+	 * The set of all names of clients in the chat room. Maintained so that we can
+	 * check that new clients are not registering name already in use.
+	 */
+
+	private static HashSet<String> names = new HashSet<String>();
+
+	/**
+	 * The set of all the print writers for all the clients. This set is kept so we
+	 * can easily broadcast messages.
+	 */
+	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+
+	/**
+	 * The appplication main method, which just listens on a port and spawns handler
+	 * threads.
+	 */
+	// To store the pointtoPoint user names with their sockets
+	private static HashMap<String, Socket> pointTopoint = new HashMap<String, Socket>();
+
+	// To store the names of multicast users
+
+	private static HashSet<String> multiCast = new HashSet<String>();
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("The chat server is running.");
+		ServerSocket listener = new ServerSocket(PORT);
+		try {
+			while (true) {
+				System.out.println("Thread is creating");
+
+				Socket socket = listener.accept();
+				System.out.println("socket is " + socket);
+				System.out.println("got the socket");
+				// Putting the relevant client socket
+				Thread handlerThread = new Thread(new Handler(socket));
+				handlerThread.start();
+			}
+		} finally {
+			listener.close();
+		}
+	}
+
+	/**
+	 * A handler thread class. Handlers are spawned from the listening loop and are
+	 * responsible for a dealing with a single client and broadcasting its messages.
+	 */
+	private static class Handler implements Runnable {
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+
+		/**
+		 * Constructs a handler thread, squirreling away the socket. All the interesting
+		 * work is done in the run method.
+		 */
+		public Handler(Socket socket) {
+			this.socket = socket;
+		}
+
+		/**
+		 * Services this thread's client by repeatedly requesting a screen name until a
+		 * unique one has been submitted, then acknowledges the name and registers the
+		 * output stream for the client in a global set, then repeatedly gets inputs and
+		 * broadcasts them.
+		 */
+		public void run() {
+			try {
+
+				// Create character streams for the socket.
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+
+				// Request a name from this client. Keep requesting until
+				// a name is submitted that is not already used. Note that
+				// checking for the existence of a name and adding the name
+				// must be done while locking the set of names.
+
+				while (true) {
+					System.out.println("came!!!");
+					out.println("SUBMITNAME");
+					name = in.readLine();
+					if (name == null) {
+						System.out.println("returned");
+						return;
+					}
+
+					// TODO: Add code to ensure the thread safety of the
+					// the shared variable 'names'
+					synchronized (names) {
+						if (!names.contains(name)) {
+							names.add(name);
+							break;
+						}
+					}
+				}
+
+				// Now that a successful name has been chosen, add the
+				// socket's print writer to the set of all writers so
+				// this client can receive broadcast messages.
+				out.println("NAMEACCEPTED");
+				synchronized (this) {
+					writers.add(out);
+					pointTopoint.put(name, socket);
+				}
+
+				// TODO: You may have to add some code here to broadcast all clients the new
+				// client's name for the task 9 on the lab sheet.
+
+				// Accept messages from this client and broadcast them.
+				// Ignore other clients that cannot be broadcasted to.
+
+				while (true) {
+					// Sending the logged in user names to client side jList(To all clients)
+					for (PrintWriter writer : writers) {
+						for (String temp : names) {
+							synchronized (this) {
+
+								writer.println("USERS" + temp);
+
+							}
+
+						}
+
+					}
+
+					String input = in.readLine();
+
+					if (input == null) {
+						return;
+
+					}
+					// sending the broadcast message to all clients
+					if (!((input.contains(">>")) || input.startsWith("Multi"))) {
+
+						for (PrintWriter writer : writers) {
+							writer.println("MESSAGE " + name + ": " + input);
+						}
+					}
+
+					// TODO: Adsd code to send a mesage to a specific client and not
+					// all clients. You may have to use a HashMap to store the sockets along
+					// with the chat client names
+
+					// Sending the point to point message to the sender and receiver
+					if (input.contains(">>")) {
+						String totString = input;
+						String inputPointToPoint = input.substring(0, input.indexOf('>'));
+
+						Iterator<Map.Entry<String, Socket>> entrySet = pointTopoint.entrySet().iterator();
+
+						while (entrySet.hasNext()) {
+							Map.Entry<String, Socket> entry = entrySet.next();
+
+							if (entry.getKey().equals(inputPointToPoint)) {
+
+								Socket socket = entry.getValue();
+								Socket socketSender = pointTopoint.get(name);
+
+								PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+								// sending to the relevant receiver
+								out.println("PointToPoint" + totString);
+								// sending to the sender
+								PrintWriter outSender = new PrintWriter(socketSender.getOutputStream(), true);
+								outSender.println("PointToPoint" + totString);
+
+							}
+						}
+
+					}
+					// Multicast messaging
+					if (input.startsWith("Multi")) {
+
+						out.println("Multicast");
+
+						while (true) {
+							String input2 = in.readLine();
+							// Adding the multicast users to the Hash set
+							if (!(input2.equals("END"))) {
+								synchronized (this) {
+									multiCast.add(input2);
+								}
+
+							}
+							// sending the multicast message to relevant parties
+							if (input2.equals("END")) {
+								try {
+									Iterator<String> itr = multiCast.iterator();
+									while (itr.hasNext()) {
+										String user = itr.next();
+										Socket socket = pointTopoint.get(user);
+										if ((socket != null)) {
+
+											PrintWriter outmultiCast = new PrintWriter(socket.getOutputStream(), true);
+
+											outmultiCast.println("MultiCastMSG" + name + ":" + input.substring(5));
+
+										}
+
+									}
+									PrintWriter sender = new PrintWriter(pointTopoint.get(name).getOutputStream(),
+											true);
+									sender.println("MultiCastMSG" + name + ":" + input.substring(5));
+									multiCast.clear();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+								break;
+							}
+						}
+					}
+
+				}
+
+			}
+			// TODO: Handle the SocketException here to handle a client closing the socket
+			catch (SocketException e) {
+				System.out.println(name + "is going down");
+
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+				// This client is going down! Remove its name and its print
+				// writer from the sets, and close its socket.
+
+				// Removing the user from other clients JLists
+				for (PrintWriter writer : writers) {
+					synchronized (this) {
+						writer.println("RemoveUsers" + name);
+
+					}
+
+				}
+				if (name != null) {
+					names.remove(name);
+				}
+
+				if (out != null) {
+					writers.remove(out);
+				}
+
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+}
